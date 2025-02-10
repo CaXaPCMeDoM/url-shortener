1dd5c863f23d3fc23d5fc8a74e148b28619d4dd2
Vladislav <114598862+CaXaPCMeDoM@users.noreply.github.com>
2025-02-10 21:45:00 +0300

    feat: add .proto

---
+++ internal/config/config.go
@@ -0,0 +1,56 @@
+package config
+
+import (
+	"fmt"
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env        string     `yaml:"env" env-default:"prod"`
+	HttpServer HttpServer `yaml:"http_server"`
+	Database   Database   `yaml:"db"`
+}
+
+type HttpServer struct {
+	Address     string        `yaml:"address" env-required:"true"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"5s"`
+	IdleTimeout time.Duration `yaml:"idleTimeout" env-default:"60s"`
+}
+
+type Database struct {
+	Host     string `yaml:"host" env-required:"true"`
+	Port     int    `yaml:"port" env-required:"true"`
+	User     string `yaml:"user" env-required:"true"`
+	Password string `yaml:"password" env-required:"true"`
+	DBName   string `yaml:"dbname" env-required:"true"`
+	SSLMode  string `yaml:"sslmode" env-default:"disable"`
+}
+
+func (db *Database) DSN() string {
+	return fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+		db.Host, db.Port, db.User, db.Password, db.DBName, db.SSLMode,
+	)
+}
+
+func MustLoad() *Config {
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH is not set")
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("Config file does not exist: %s", configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		log.Fatalf("Cannot read config: %s", err)
+	}
+
+	return &cfg
+}
